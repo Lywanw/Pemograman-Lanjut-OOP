a3b7c9cde0cfc553c62e235051c84db267e656ee
Lywanw <12450113341@students.uin-suska.ac.id>
2025-06-08 20:42:32 +0700

    Add files via upload

---
+++ Pencarian dan Pengurutan/src/Stat.java
@@ -0,0 +1,69 @@
+public class Stat extends Pokemon {
+
+    protected int total;
+    protected int hp;
+    protected int attack;
+    protected int defense;
+    protected int sp_attack;
+    protected int sp_defense;
+    protected int speed;
+
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSp_attack() {
+        return sp_attack;
+    }
+
+    public void setSp_attack(int sp_attack) {
+        this.sp_attack = sp_attack;
+    }
+
+    public int getSp_defense() {
+        return sp_defense;
+    }
+
+    public void setSp_defense(int sp_defense) {
+        this.sp_defense = sp_defense;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
+
+
